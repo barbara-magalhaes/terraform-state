664fc9bcc9c03d2c46701879b5f74156e9bfa37e
Babs <61755205+barbara-magalhaes@users.noreply.github.com>
2025-04-06 22:55:50 -0300

    Delete AWS-VPC directory

--- AWS-VPC/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "subnet_id" {
-value = aws__subnet.subnet.id
-}
-
-output "security_group_id" {
-  description = "ID da Security group criada na AWS"
-  value = aws_security_group.security_group.id
-}
